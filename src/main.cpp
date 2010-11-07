75b019f31da875494da140e991501d783ef8ac34
ivan.kristo <ivan.kristo@6100b896-52a2-3748-62e0-4fb15095d2ea>
2010-11-07 15:46:12 +0000

    Dodan filter za binarizaciju slika, ispravljeno zapisivanje gray slika.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include "imageIO.h"
 #include "openCVJpegIO.h"
+#include "binaryFilter.h"
 
 using namespace std;
 
 int main() {
+	
 	cout << "Hello\n";
 	ImageIO *io = new JpegIO();
 	ColorImage *img = 0;
@@ -15,5 +17,10 @@ int main() {
 	char *fileNameexit = (char*)fileNameSexit.c_str();
 	img = io->read(fileName);
 	io->write(img, fileNameexit);
+
+	BinaryFilter* bf = new BinaryFilter(150);
+	GrayImage* fImage = bf->applyFilterC2G(img);
+	string binImgPath = "testBin.jpg";
+	io->write(fImage, (char*)binImgPath.c_str());
 	return 1;
 }
